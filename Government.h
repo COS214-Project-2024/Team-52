c2a04345e6d59aa80e59bce519bb90f081697909
Jay-9-9-9 <u22625926@tuks.co.za>
2024-10-28 19:46:38 +0200

    adding some functions for Government class

--- Government.h
+++ Government.h
@@ -1,14 +1,40 @@
 #ifndef GOVERNMENT_H
 #define GOVERNMENT_H
 
+#include "Citizen.h"
+
+#include <string>
+#include <vector>
+
+using namespace std ;
+
 class Government {
   private :
-    GovernmentMediator* mediator ;
-    Government* backup ;
-    CityGrowthManager* cityGrowthManager ;
-    vector<Citizen> citizens ;
-    vector<Building> buildings ;
+      GovernmentMediator* mediator ;
+      Government* backup ;
+      CityGrowthManager* cityGrowthManager ;
+      vector<Citizen> citizens ;
+      vector<Building> buildings ;
   public :
+    Government(double cRate, double bRate, vector<Citizen*>)
+
+    // Command
+    void collectCitizenTax() ;
+    void collectBuildingTax();
+    void collectCityTax();
+
+    // Composite
+    void moveIn(int numPeople, string reason);
+    void addCitizen(Citizen* c);
+    void addBuilding(Building* b);
+
+    // Memento
+    GovernmentMemento* saveState();
+    void restoreState();
+    void displayState();
+
+
+
 }
 
 
