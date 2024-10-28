eeb0170910b0dfaae9dbc6a269816749e9ecde49
Jay-9-9-9 <u22625926@tuks.co.za>
2024-10-28 19:29:50 +0200

    Create Government.h

---
+++ Government.h
@@ -0,0 +1,17 @@
+#ifndef GOVERNMENT_H
+#define GOVERNMENT_H
+
+class Government {
+  private :
+    GovernmentMediator* mediator ;
+    Government* backup ;
+    CityGrowthManager* cityGrowthManager ;
+    vector<Citizen> citizens ;
+    vector<Building> buildings ;
+  public :
+}
+
+
+
+
+#endif
