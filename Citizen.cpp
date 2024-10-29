c95cffc47869fccd0cd71d313b525743f6019beb
Lineo-dev01 <u23604043@tuks.co.za>
2024-10-29 13:59:16 +0200

    Added implementation of Citizen
    added the implementation of citizen

--- Citizen.cpp
+++ Citizen.cpp
@@ -1,1 +1,34 @@
 #include "Citizen.h"
+
+#include <iostream>
+
+Citizen::Citizen() : name(""), ID(0), age(0), income(0.0), satisfaction(0), isEmployed(false) {}
+
+void Citizen::setName(const std::string& name) { 
+    this->name = name; 
+}
+
+void Citizen::setID(int ID) {
+     this->ID = ID; 
+}
+
+void Citizen::setAge(int age) {
+     this->age = age; 
+}
+
+void Citizen::setIncome(double income) {
+     this->income = income; 
+}
+
+void Citizen::setSatisfaction(int satisfaction) {
+     this->satisfaction = satisfaction; 
+}
+void Citizen::setEmploymentStatus(bool isEmployed) {
+     this->isEmployed = isEmployed;
+}
+
+void Citizen::display() const {
+    std::cout << "Name: " << name << ", ID: " << ID << ", Age: " << age
+              << ", Income: " << income << ", Satisfaction: " << satisfaction
+              << ", Employed: " << (isEmployed ? "Yes" : "No") << "\n";
+}
