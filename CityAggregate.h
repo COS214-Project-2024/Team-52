1e7be595f7456c05bf608784f2a5d1dc62dbb56d
Tebatso Sophy Mahlathini <u22611704@tuks.co.za>
2024-10-29 22:47:13 +0200

    implemented citizenIterator ,buildingIterator, city classes

--- CityAggregate.h
+++ CityAggregate.h
@@ -3,12 +3,14 @@
 
 #include <iostream>
 using namespace std;
+#include "CitizenIterator.h"
 
 class CityAggregate{
     private:
 
     public:
-    
+    virtual CityIterator* createBuildingIterator()=0;
+    virtual CityIterator* createCitizenInterator()=0;
 };
 
 #endif
