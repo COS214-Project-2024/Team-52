8f8eb3eeca2b42f62b4692d079f86b81dad57757
Lineo-dev01 <u23604043@tuks.co.za>
2024-10-29 14:38:05 +0200

    Added to interface for building Citizen

--- CitizenBuilder.h
+++ CitizenBuilder.h
@@ -3,12 +3,18 @@
 
 #include <iostream>
 using namespace std;
+#include "Citizen.h"
 
 class CitizenBuilder{
-    private:
-
-    public:
-    
+ public:
+    virtual ~CitizenBuilder();
+    virtual void setName(const std::string& name) = 0;
+    virtual void setID(int ID) = 0;
+    virtual void setAge(int age) = 0;
+    virtual void setIncome(double income) = 0;
+    virtual void setSatisfaction(int satisfaction) = 0;
+    virtual void setEmploymentStatus(bool isEmployed) = 0;
+    virtual Citizen* getCitizen() = 0;
 };
 
 #endif
