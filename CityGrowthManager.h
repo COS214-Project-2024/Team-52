083a84b015d42840c56d99b0b58519cf0e787f19
Jay-9-9-9 <u22625926@tuks.co.za>
2024-10-30 20:01:29 +0200

    Some Changes on City Growth Manager

--- CityGrowthManager.h
+++ CityGrowthManager.h
@@ -2,7 +2,25 @@
 #define CITYGROWTHMANAGER_H
 
 class CityGrowthManager{
-
+    private :
+        int population ;
+        int numberOfBuildings ;
+        double economicGrowthRate ;
+    public :
+        CityGrowthManager(int pop, int numOfBuildings, double econG_rate);
+        //setters
+        void setPopulation(int pop);
+        void setNumOfBuildings(int num);
+        void setEconGrowthrate(double rate);
+        //Formulas + stats
+        void evaluatePopulationGrowth();
+        void evaluateBuildingExpansion();
+        void evaluateEconomicGrowth();
+        void evaluateGrowth();
+        //getters
+        int getPopulation();
+        int getNumOfBuildings();
+        double getEconGrowthRate();
 };
 
 #endif
